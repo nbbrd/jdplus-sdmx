bed602a1dab34219989c063c3cce053adda5bd02
Philippe Charles <philippe.charles@nbb.be>
2023-03-23 13:38:29 +0100

    Improve module-info names

---
+++ jd3-sdmx-base/jd3-sdmx-base-parent/jd3-sdmx-base-core/src/main/java/module-info.java
@@ -0,0 +1,19 @@
+module jdplus.sdmx.base.core {
+
+    requires static lombok;
+    requires static nbbrd.design;
+    requires static nbbrd.service;
+    requires static org.checkerframework.checker.qual;
+
+    requires sdmxdl.api;
+    requires jdplus.toolkit.base.tsp;
+    requires nbbrd.io.base;
+
+    exports demetra.tsp.extra.sdmx;
+    exports demetra.tsp.extra.sdmx.file;
+    exports demetra.tsp.extra.sdmx.web;
+
+    provides demetra.timeseries.TsProvider with
+            demetra.tsp.extra.sdmx.file.SdmxFileProvider,
+            demetra.tsp.extra.sdmx.web.SdmxWebProvider;
+}
