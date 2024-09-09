9a154d3110e4e02a97689fa492c2d096da218416
Philippe Charles <philippe.charles@nbb.be>
2024-09-09 09:36:04 +0200

    Fix API changes

--- jdplus-sdmx-desktop/jdplus-sdmx-desktop-plugin/src/main/java/jdplus/sdmx/desktop/plugin/Toggle.java
+++ jdplus-sdmx-desktop/jdplus-sdmx-desktop-plugin/src/main/java/jdplus/sdmx/desktop/plugin/Toggle.java
@@ -7,10 +7,14 @@ public enum Toggle {
     DEFAULT, DISABLE, ENABLE;
 
     public void applyTo(Properties properties, CharSequence key) {
+        applyTo(properties, key, "false", "true");
+    }
+
+    public void applyTo(Properties properties, CharSequence key, String disableValue, String enableValue) {
         switch (this) {
             case DEFAULT -> properties.remove(key.toString());
-            case DISABLE -> properties.setProperty(key.toString(), Boolean.toString(false));
-            case ENABLE -> properties.setProperty(key.toString(), Boolean.toString(true));
+            case DISABLE -> properties.setProperty(key.toString(), disableValue);
+            case ENABLE -> properties.setProperty(key.toString(), enableValue);
         }
     }
 }
